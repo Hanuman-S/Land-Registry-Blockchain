910a1abd18ae39f1f845e4434a4036102951763e
Hanuman-S <hanuman212006@gmail.com>
2026-04-14 18:10:15 +0530

    added a server to backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const registryRoutes = require("./routes/registry");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", registryRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Backend running 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("🚀 Server running on http://localhost:3000");
+});
